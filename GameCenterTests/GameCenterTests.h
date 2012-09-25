1684b6b95f82c7003f0d2c2fe645efe8755fc403
Matthew Radke <themoderngrad@gmail.com>
2012-09-25 09:04:33 -0400

    Initial Commit

---
+++ GameCenterTests/GameCenterTests.h
@@ -0,0 +1,13 @@
+//
+//  GameCenterTests.h
+//  GameCenterTests
+//
+//  Created by matthew radke on 9/25/12.
+//  Copyright (c) 2012 matthew radke. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GameCenterTests : SenTestCase
+
+@end
---
+++ GameCenterTests/GameCenterTests.m
@@ -0,0 +1,32 @@
+//
+//  GameCenterTests.m
+//  GameCenterTests
+//
+//  Created by matthew radke on 9/25/12.
+//  Copyright (c) 2012 matthew radke. All rights reserved.
+//
+
+#import "GameCenterTests.h"
+
+@implementation GameCenterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GameCenterTests");
+}
+
+@end
