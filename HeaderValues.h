41abddef02fee37ede9688f39d65e356b21b98d9
Matthew Radke <themoderngrad@gmail.com>
2012-09-25 09:15:44 -0400

    game files

---
+++ HeaderValues.h
@@ -0,0 +1,21 @@
+//
+//  HeaderValues.h
+//  GameCenter
+//
+//  Created by matthew radke on 9/25/12.
+//  Copyright (c) 2012 matthew radke. All rights reserved.
+//
+
+//Leaderboard Category IDs
+#define kLeaderboardID @"100"
+//Achievement IDs
+#define kAchievement100points @"100_points"
+#define kAchievement200points @"200_points"
+#define kAchievement300points @"300_points"
+#define kAchievement400points @"400_points"
+#define kAchievement500points @"500_points"
+#define kAchievement600points @"600_points"
+#define kAchievement700points @"700_points"
+#define kAchievement800points @"800_points"
+#define kAchievement900points @"900_points"
+#define kAchievement1000points @"1000_points"
